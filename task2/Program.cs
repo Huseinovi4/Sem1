42b29ad463186c6957b6996497dafc3b0989dfe7
Aleksey <lexandreeev@gmail.com>
2023-01-18 20:10:52 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите первое число");
+int firstNum = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите второе число:");
+int secondNum = int.Parse(Console.ReadLine()!);
+if (firstNum == secondNum*secondNum){
+  Console.WriteLine("является квадратом второго чила");
+}
+else{
+  Console.WriteLine("не является квадратом второго числа");
+}
