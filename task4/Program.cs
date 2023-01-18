a344db15d2a6a8b75700ed1c65b20e5df5be6af5
Aleksey <lexandreeev@gmail.com>
2023-01-18 21:20:12 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число:");
+int firstNumber = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:");
+int secondNumber = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число:");
+int thirdNumber = int.Parse(Console.ReadLine()!);
+int maxNumber = firstNumber;
+if(secondNumber>firstNumber){
+  maxNumber=secondNumber;
+}
+if(thirdNumber>maxNumber){
+  maxNumber=thirdNumber;
+}
+else{
+
+}
+Console.WriteLine($"Максимальное из трех чисел:{maxNumber}");
