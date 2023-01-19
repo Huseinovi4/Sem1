339c92b4303a2ebc07a86ee1fbeef9b215b1294b
Aleksey <lexandreeev@gmail.com>
2023-01-19 17:29:05 +0300

    add task5

---
+++ task5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+if(number % 2 == 0){
+  Console.WriteLine("число четное");
+}
+else{
+  Console.WriteLine("число не четное");
+}
