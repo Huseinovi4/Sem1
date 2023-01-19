a8766074da961d3f3b40071185ae49dbc8491596
Aleksey <lexandreeev@gmail.com>
2023-01-19 19:01:53 +0300

    add task8

---
+++ task8/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введи трёхзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+string stringNumber = Convert.ToString(number);
+Console.WriteLine("вторая цифра этого числа:"+stringNumber[1]);
