89f57c0d8300d51bd6340c2bd771e4d138c45585
Aleksey <lexandreeev@gmail.com>
2023-01-20 19:47:54 +0300

    add task11

---
+++ task11/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Задайте число:");
+int firstNumber = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число для проверки :");
+int secondNumber = int.Parse(Console.ReadLine()!);
+if(firstNumber%secondNumber==0){
+  Console.WriteLine("Число кратно заданному!");
+}
+else{
+  int remainder = firstNumber%secondNumber;
+  Console.WriteLine($"Остаток:{remainder}");
+}
