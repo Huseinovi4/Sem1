4f433e291a1480f6b6b5d6f09fe9576768d021b3
Aleksey <lexandreeev@gmail.com>
2023-01-20 19:28:21 +0300

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехначное число:");
+int number = int.Parse(Console.ReadLine()!);
+int p = number /100;
+int r =number%10;
+Console.WriteLine($"Получите двухзначное:{p}{r}");
